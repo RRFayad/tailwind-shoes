6ab464087d0cc9808d0f55d7a74d89d5853eb505
RRFayad <renan.r.fayad@gmail.com>
2023-11-27 20:26:02 -0300

    Gradient Responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,7 +8,7 @@ const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 function Nav() {
   const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
   return (
-    <nav className="flex flex-wrap items-center justify-between">
+    <nav className="relative z-10 flex flex-wrap items-center justify-between">
       {/* Logo */}
       <a href="#">
         <NikeLogo className="h-20 w-20" />
@@ -27,10 +27,12 @@ function Nav() {
         } w-full lg:block lg:w-auto`}
       >
         <ul className=" flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 lg:flex-row lg:space-x-8 lg:border-none lg:bg-transparent">
-          {ROUTES.map((route) => (
+          {ROUTES.map((route, i) => (
             <li
               key={route}
-              className=" cursor-pointer rounded-lg  px-3 py-2 font-sans text-lg hover:bg-gray-100 sm:first:bg-blue-500 sm:first:text-white lg:first:bg-transparent lg:first:text-blue-500"
+              className={`cursor-pointer rounded-lg px-3 py-2 font-sans text-lg hover:bg-gray-100 sm:first:bg-blue-500 sm:first:text-white  lg:first:bg-transparent lg:first:text-blue-500 lg:hover:bg-transparent lg:hover:text-blue-500 ${
+                (i === 3 || i === 4) && "lg:text-white"
+              } `}
             >
               {route}
             </li>
@@ -38,7 +40,7 @@ function Nav() {
         </ul>
       </div>
       <div className="fixed bottom-4 left-4 lg:static">
-        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+        <div className="flex-center h-12 w-12 cursor-pointer rounded-full bg-white shadow-md lg:mr-8">
           <TbShoppingBag />
         </div>
       </div>

d7c6c6f8f6ca35209954bd8600531ad25a0d7a41
RRFayad <renan.r.fayad@gmail.com>
2023-12-04 09:00:04 -0300

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Nav from "./components/nav";
 import ShoeDetail from "./components/ShoeDetail";
+import Card from "./components/Card";
+import { SHOE_LIST } from "./constant";
 
 function App() {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
-      <Nav />
-      <ShoeDetail />
+      {/* <Nav />
+      <ShoeDetail /> */}
+      <Card item={SHOE_LIST[0]} />
     </div>
   );
 }
